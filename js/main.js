d6a5a46a40e65253e59c4b2069a12c2f10c5385c
Sara Bento de Castro <denominatu@hotmail.com>
2018-06-17 17:10:34 +0200

    made index page responsive and added styles

--- js/main.js
+++ js/main.js
@@ -140,25 +140,30 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.alt = "photo of" + restaurant.name + "restaurant";
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const textHolder = document.createElement('div');
+  textHolder.setAttribute("class","restaurant-textholder");
+  li.append(textHolder);
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  textHolder.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  textHolder.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  textHolder.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  textHolder.append(more)
 
   return li
 }
